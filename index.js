656fa780ce53ab7fe28873c893bddea6e9ada012
antoninaorobets <pani.antonina.don@gmail.com>
2021-11-23 12:09:34 -0500

    commit tree 18a96d925cc6202b290fefd72f03ed5a8aaf4d60

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-function saturdayFun(activity="roller-skate") {
-  return `This Saturday, I want to ${activity}!`
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
 }
 
-let mondayWork = function(activity="go to the office") {
+const mondayWork = function (activity = "go to the office") {
   return `This Monday, I will ${activity}.`
 }
 
-let wrapAdjective = function(style="*") {
-  return function(adjective="special") {
-    return `You are ${style}${adjective}${style}!`
+function wrapAdjective (string="*") {
+  return function (text = "special"){
+      return `You are ${string}${text}${string}!`;
   }
 }
+
+wrapAdjective("%")("a dedicated programmer");
